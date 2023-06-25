558cd6bb3854bf30435740142b7b6618547d34d4
Sabin Marcu <sabinmarcu@gmail.com>
2023-06-25 19:25:55 +0300

    feat: configure fonts and paddings for window

--- config/window.lua
+++ config/window.lua
@@ -2,9 +2,18 @@ local wezterm = require("wezterm")
 
 return function(config)
 	config.window_frame = {
-		font = wezterm.font({ family = "Roboto", weight = "Bold" }),
+		font = wezterm.font_with_fallback({
+			"MonoLisa Variable",
+			"Symbols Nerd Font Mono",
+		}),
 		font_size = 12.0,
 		active_titlebar_bg = "#333333",
 		inactive_titlebar_bg = "#333333",
+	}
+	config.window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
 	}
 end
