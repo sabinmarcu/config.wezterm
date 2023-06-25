77be2842b5dc59589a2b1bac15ad5d2bfc740a5d
Sabin Marcu <sabinmarcu@gmail.com>
2023-06-25 22:17:03 +0300

    feat: streamline keybinds for tmux

--- config/keys.lua
+++ config/keys.lua
@@ -2,24 +2,11 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 local tmuxKeys = {
-	{ key = "e", mods = "CMD", action = act.SendString('\x02"') }, -- split tmux window vertically
-	{ key = "e", mods = "CMD|SHIFT", action = act.SendString("\x02%") }, -- split tmux window horizontally
-	{ key = "f", mods = "CMD|SHIFT", action = act.SendString("\x02\x5b\x2f") }, -- start tmux search mode
-	{ key = "j", mods = "CMD", action = act.SendString("\x02\x54") }, -- open t - tmux smart session manager
-	{ key = "k", mods = "CMD", action = act.SendString("\x02s") }, -- open interactive tmux session client
-	{ key = "x", mods = "CMD", action = act.SendString("\x1b\x3a\x71\x0a") }, -- quit vim
-	{ key = ";", mods = "CMD", action = act.SendString("\x02?") },
-	{ key = "s", mods = "CMD", action = act.SendString("\x1b\x3a\x77\x0a") }, -- save vim buffer
-	{ key = "t", mods = "CMD", action = act.SendString("\x02c") }, -- create a new tmux window
-	{ key = "w", mods = "CMD", action = act.SendString("\x02x") }, -- kill the current pane
-	{ key = "z", mods = "CMD", action = act.SendString("\x02z") }, -- toggle zoom state of the current tmux pane
-	{ key = "Tab", mods = "CTRL", action = act.SendString("\x02n") }, -- switch to next tmux window
-	{ key = "Tab", mods = "CTRL|SHIFT", action = act.SendString("\x02p") }, -- switch to previous tmux window
-	{ key = ",", mods = "CMD", action = act.SendString("\x02,") }, -- rename the current tmux window
+	--- TMUX
 	{ key = "l", mods = "CMD|SHIFT", action = act.SendString("\x02l") }, -- right pane
 	{ key = "h", mods = "CMD|SHIFT", action = act.SendString("\x02h") }, -- left pane
-	{ key = "j", mods = "CMD|SHIFT", action = act.SendString("\x02j") }, -- left pane
-	{ key = "k", mods = "CMD|SHIFT", action = act.SendString("\x02k") }, -- left pane
+	{ key = "j", mods = "CMD|SHIFT", action = act.SendString("\x02j") }, -- top pane
+	{ key = "k", mods = "CMD|SHIFT", action = act.SendString("\x02k") }, -- bottom pane
 	{ key = "[", mods = "CMD|SHIFT", action = act.SendString("\x02p") }, -- switch to next tmux window
 	{ key = "]", mods = "CMD|SHIFT", action = act.SendString("\x02n") }, -- switch to previous tmux window
 	{ key = ";", mods = "CMD", action = act.SendString("\x02=") }, -- enter the tmux command prompt
@@ -33,6 +20,23 @@ local tmuxKeys = {
 	{ key = "7", mods = "CMD", action = act.SendString("\x027") }, --                ... 7
 	{ key = "8", mods = "CMD", action = act.SendString("\x028") }, --                ... 8
 	{ key = "9", mods = "CMD", action = act.SendString("\x029") }, --                ... 9
+	{ key = "Tab", mods = "CTRL", action = act.SendString("\x02n") }, -- switch to next tmux window
+	{ key = "Tab", mods = "CTRL|SHIFT", action = act.SendString("\x02p") }, -- switch to previous tmux window
+	{ key = ",", mods = "CMD", action = act.SendString("\x02,") }, -- rename the current tmux window
+	{ key = "z", mods = "CMD|SHIFT", action = act.SendString("\x02z") }, -- toggle zoom state of the current tmux pane
+	{ key = "k", mods = "CMD", action = act.SendString("\x02s") }, -- open interactive tmux session client
+	{ key = "t", mods = "CMD", action = act.SendString("\x02c") }, -- create a new tmux window
+	{ key = "w", mods = "CMD", action = act.SendString("\x02x") }, -- kill the current pane
+	{ key = "e", mods = "CMD", action = act.SendString('\x02"') }, -- split tmux window vertically
+	{ key = "e", mods = "CMD|SHIFT", action = act.SendString("\x02%") }, -- split tmux window horizontally
+	{ key = "f", mods = "CMD|SHIFT", action = act.SendString("\x02\x5b\x2f") }, -- start tmux search mode
+	{ key = "j", mods = "CMD", action = act.SendString("\x02\x54") }, -- open t - tmux smart session manager
+
+	--- NVIM
+	{ key = "s", mods = "CMD", action = act.SendString("\x1b\x3a\x77\x0a") }, -- save vim buffer
+	{ key = "z", mods = "CMD|SHIFT", action = act.SendString("\x12") }, -- reundo in nvim
+	{ key = "z", mods = "CMD", action = act.SendString("\x1bu") }, -- undo in nvim
+	{ key = "x", mods = "CMD", action = act.SendString("\x1b\x3a\x71\x0a") }, -- quit vim
 }
 
 -- local weztermKeys = {
