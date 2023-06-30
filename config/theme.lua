b40b3e4a44fb13cc76d43e50afc82c8a3a62152e
Sabin Marcu <sabinmarcu@gmail.com>
2023-06-30 08:58:24 +0300

    fix: remove window_frame config

--- config/theme.lua
+++ config/theme.lua
@@ -1,11 +1,6 @@
 local wezterm = require("wezterm")
 
 return function(config)
-	config.window_frame = {
-		font = wezterm.font({ family = "MonoLisa Nerd Font", weight = "Bold" }),
-		font_size = 12.0,
-	}
-
 	local status, theme = pcall(require, "theme")
 	if status then
 		theme(config)
