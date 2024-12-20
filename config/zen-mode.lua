de5ed8c9cb76169079d534faa70fb4e34679e0b5
Sabin Marcu <sabin.marcu@platform24.com>
2024-12-20 19:54:56 +0200

    feat: update zen-mode support

--- config/zen-mode.lua
+++ config/zen-mode.lua
@@ -1,19 +1,35 @@
 local wezterm = require("wezterm")
 
 return function()
+	local previousIncrement = nil
+
+	local mutateFontSize = function(window, pane, increment)
+		local value = increment
+		while value ~= 0 do
+			if value > 0 then
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				value = value - 1
+			else
+				window:perform_action(wezterm.action.DecreaseFontSize, pane)
+				value = value + 1
+			end
+		end
+	end
+
 	wezterm.on("user-var-changed", function(window, pane, name, value)
 		local overrides = window:get_config_overrides() or {}
 		if name == "ZEN_MODE" then
 			local incremental = value:find("+")
 			local number_value = tonumber(value)
 			if incremental ~= nil then
-				while number_value > 0 do
-					window:perform_action(wezterm.action.IncreaseFontSize, pane)
-					number_value = number_value - 1
+				if number_value > 0 then
+					previousIncrement = number_value
+					mutateFontSize(window, pane, number_value)
 				end
 			elseif number_value < 0 then
-				window:perform_action(wezterm.action.ResetFontSize, pane)
+				mutateFontSize(window, pane, 0 - previousIncrement)
 				overrides.font_size = nil
+				previousIncrement = nil
 			else
 				overrides.font_size = number_value
 			end
